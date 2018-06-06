df1e87a9fff74cb497664bbd672de3a3ca03699d
Young Eun Kim <aakim0907@gmail.com>
2018-06-06 16:19:28 -0700

    set up routes and App components

---
+++ react/client/src/components/Article/Form/index.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import axios from 'axios';
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '',
+      body: '',
+      author: ''
+    };
+
+    this.handleChangeField = this.handleChangeField.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeField(key, event) {
+    this.setState({
+      [key]: event.target.value
+    });
+  }
+
+  handleSubmit() {
+    const { title, body, author } = this.state;
+
+    return axios.post('http://localhost:8000/api/articles', {
+      title,
+      body,
+      author
+    }).then(() => this.setState({ title: '', body: '', author: '' }));
+  }
+  
+  render() {
+    const { title, body, author } = this.state;
+
+    return (
+      <div className="col-12 col-lg-6 offset-lg-3">
+        <input
+          onChange={ev => this.handleChangeField('title', ev)} 
+          value={title}
+          className="form-control my-3" 
+          placeholder="Article Title" />
+        <textarea 
+          onChange={ev => this.handleChangeField('body', ev)} 
+          value={body}
+          className="form-control my-3" 
+          placeholder="Article Description">
+        </textarea>
+        <input 
+          onChange={ev => this.handleChangeField('author', ev)} 
+          value={author}
+          className="form-control my-3" 
+          placeholder="Article Author" />
+
+        <button 
+          className="btn btn-primary float-right"
+          onClick={this.handleSubmit}>Submit</button>
+      </div>
+    );
+  }
+}
+
+export default Form;
